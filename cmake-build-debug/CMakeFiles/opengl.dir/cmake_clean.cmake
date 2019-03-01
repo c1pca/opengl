fb0b147d806eb3ce2e8a2a1d021ee31f9698f6b2
Yura Tkachyshyn <ytkachyshyn@gmail.com>
2019-03-01 02:28:00 +0200

    begin

---
+++ cmake-build-debug/CMakeFiles/opengl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl.dir/main.c.o"
+  "opengl.pdb"
+  "opengl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
